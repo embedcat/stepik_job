6e855926ab3547facf8319e2bf7ca1cd5dffb012
hotclimbing <zzerocks@gmail.com>
2020-10-14 00:53:56 +0300

    Работа над регистрацией и входом

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,15 +1,23 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from vacancies.views import MainView, VacanciesAllView, VacancyView, \
-    CompanyView, SpecialitiesView, CompaniesAllView, about_view, CustomLoginView, RegisterView
+    CompanyView, SpecialitiesView, CompaniesAllView, \
+    MyCompanylVacancyListView, MyCompanyVacancyEditView, MyCompanyView, VacancySendApplicationView, \
+    about_view, CustomLoginView, RegisterView
 
 urlpatterns = [
-    path('', MainView.as_view(), name='Main view'),
-    path('vacancies/', VacanciesAllView.as_view(), name='All vacancies'),
-    path('vacancies/cat/<str:spec_code>', SpecialitiesView.as_view(), name='Speciality view'),
-    path('companies/<int:company_id>', CompanyView.as_view(), name='Company card'),
-    path('vacancies/<int:vacancy_id>', VacancyView.as_view(), name="Vacancy view"),
-    path('companies/', CompaniesAllView.as_view(), name='All companies'),
-    path('about/', about_view, name='About View'),
-    path('login/', CustomLoginView.as_view(), name='Login View'),
-    path('register/', RegisterView.as_view(), name='Register View'),
+    path('', MainView.as_view(), name='main'),
+    path('vacancies/', VacanciesAllView.as_view(), name='all_vacancies'),
+    path('vacancies/cat/<str:spec_code>/', SpecialitiesView.as_view(), name='specialty'),
+    path('companies/<int:company_id>/', CompanyView.as_view(), name='company_card'),
+    path('vacancies/<int:vacancy_id>/', VacancyView.as_view(), name='vacancy'),
+    path('companies/', CompaniesAllView.as_view(), name='all_companies'),
+    path('about/', about_view, name='about'),
+    path('vacancies/<int:vacancy_id>/send/', VacancySendApplicationView.as_view(), name='vacancy_send'),
+    path('mycompany/', MyCompanyView.as_view(), name='mycompany'),
+    path('mycompany/vacancies/', MyCompanylVacancyListView.as_view(), name='mycompany_vacancy_list'),
+    path('mycompany/vacancies/<int:vacancy_id>/', MyCompanyVacancyEditView.as_view(), name='mycompany_vacancy'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- vacancies/views.py
+++ vacancies/views.py
@@ -6,76 +6,109 @@ from .models import Specialty, Vacancy, Company
 
 
 class MainView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         specs = Specialty.objects.all()
         companies = Company.objects.all()
         return render(request=request, template_name='vacancies/index.html',
                       context={
-                          'title': '',
+                          'title': 'Джуманджи',
                           'specs': specs,
                           'companies': companies,
                       })
 
 
 class SpecialitiesView(View):
-    def get(self, request, spec_code, *args, **kwargs):
+    def get(self, request, spec_code):
         spec = Specialty.objects.get(code=spec_code)
         return render(request=request, template_name='vacancies/search.html',
                       context={
-                          'title': f'Вакансии {spec.title} | ',
+                          'title': f'Вакансии {spec.title}',
                           'spec': spec,
                       })
 
 
 class CompanyView(View):
-    def get(self, request, company_id, *args, **kwargs):
+    def get(self, request, company_id):
         company = Company.objects.get(id=company_id)
         return render(request=request, template_name='vacancies/company.html',
                       context={
-                          'title': f'Компания {company.name} | ',
+                          'title': f'Компания {company.name}',
                           'company': company,
                       })
 
 
 class VacanciesAllView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         specs = Specialty.objects.all()
         return render(request=request, template_name='vacancies/vacancies.html',
                       context={
-                          'title': 'Вакансии | ',
+                          'title': 'Вакансии',
                           'specs': specs,
                       })
 
 
 class VacancyView(View):
-    def get(self, request, vacancy_id, *args, **kwargs):
+    def get(self, request, vacancy_id):
         vacancy = Vacancy.objects.get(id=vacancy_id)
         return render(request=request, template_name='vacancies/vacancy.html',
                       context={
-                          'title': 'Вакансия | ',
+                          'title': 'Вакансия',
                           'vacancy': vacancy,
                       })
 
 
 class CompaniesAllView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         companies = Company.objects.all()
         return render(request=request, template_name='vacancies/companies.html',
                       context={
-                          'title': 'Лучшие компании | ',
+                          'title': 'Лучшие компании',
                           'companies': companies,
                       })
 
 
+class MyCompanyView(View):
+    def get(self, request):
+        return render(request=request, template_name='vacancies/company-create.html',
+                      context={
+                          'title': 'Моя компания',
+                      })
+
+
+class MyCompanylVacancyListView(View):
+    def get(self, request):
+        return render(request=request, template_name='vacancies/vacancy-list.html',
+                      context={
+                          'title': 'Вакансии компании',
+                      })
+
+
+class MyCompanyVacancyEditView(View):
+    def get(self, request, vacancy_id):
+        return render(request=request, template_name='vacancies/vacancy-edit.html')
+
+
+class VacancySendApplicationView(View):
+    def get(self, request, vacancy_id):
+        return render(request=request, template_name='vacancies/sent.html',
+                      context={
+                          'title': 'Отклик отправлен',
+                          'vacancy_id': int(vacancy_id),
+                      })
+
+
 class CustomLoginView(LoginView):
-    redirect_authenticated_user = False
+    redirect_authenticated_user = True
     template_name = 'vacancies/login.html'
 
 
 class RegisterView(CreateView):
     form_class = UserCreationForm
-    success_url = 'login'
+    success_url = '/login'
     template_name = 'vacancies/register.html'
+    extra_context = {
+        'title': 'Регистрация',
+    }
 
 
 def about_view(request):
